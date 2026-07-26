97c009c4bf5b9c42e0eae3ea789bffee77e792e2
victortab21 <156722121+victortab21@users.noreply.github.com>
2026-07-26 22:58:48 +0200

    Update Database.h

--- include/Database.h
+++ include/Database.h
@@ -5,7 +5,7 @@
 
 class Database {
 private:
-    // Aquí vive la memoria de nuestro motor: un mapa Hash O(1)
+   
     std::unordered_map<std::string, std::string> storage;
     std :: mutex mtx;
 
@@ -13,7 +13,7 @@ public:
     Database() = default;
     ~Database() = default;
 
-    // Las 3 operaciones fundamentales del MVP
+
     void set(const std::string& key, const std::string& value);
     std::string get(const std::string& key);
     bool remove(const std::string& key);
